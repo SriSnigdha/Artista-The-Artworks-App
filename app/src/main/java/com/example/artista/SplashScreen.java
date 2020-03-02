ee4803daa45f8c36375cb1171be333af2845955c
SriSnigdha <srisnigdha14@gmail.com>
2020-03-02 12:31:29 +0530

    Made changes to Splash Screen

--- app/src/main/java/com/example/artista/SplashScreen.java
+++ app/src/main/java/com/example/artista/SplashScreen.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.artista.MainActivity;
 import com.example.artista.R;
@@ -14,7 +16,11 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
